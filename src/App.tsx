a8e0f21ecb703f4dc61170d93cc4a33796e39ce2
phuongdung10 <92843550+phuongdung10@users.noreply.github.com>
2022-05-10 10:38:07 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,41 @@
 import React from 'react';
-import Login from "././components/Login";
-import Shop from "././components/Shop";
+import Login from "./components/Login";
+import Shop from "./components/Shop";
 import Cart from "./components/Cart";
 import Profile from "./components/Profile"
 import Product from './components/Product';
 import Filter from './components/Filter';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Redirect, Switch, Link } from 'react-router-dom';
 import MyShop from './components/MyShop';
 import MyCart from './components/MyCart';
 import CartHeader from './components/CartHeader';
+import ProtectedRoute from './ProtectedRoute';
 
 
 
 function App() {
   return (
     <Switch>
-      <Route path='/'>
-        <Login />
-         <Route path='/shop'>
-          <Shop />
-        </Route>
-        <Route path='/filter'>
-          <Filter />
-        </Route>
-        <Route path='/product'>
-          <Product />
-        </Route>
-        </Route>
+      <Route
+        path="/"
+        exact
+        component={Login}
+      />
+      {/* <Route path='/'>
+        <Login /> */}
+      <Route path='/shop'>
+        <Shop />
+      </Route>
+      <Route path='/filter'>
+        <Filter />
+      </Route>
+      <Route path='/product'>
+        <Product />
+      </Route>
+      {/* </Route> */}
+
+
+
       {/* <Product /> */}
 
     </Switch>
